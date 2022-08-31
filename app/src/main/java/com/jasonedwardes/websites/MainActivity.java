04e0f815296106f97fc6899c4260196ffe39ae67
Jason Edwardes <jasonedwardes@gmail.com>
2022-08-31 21:40:02 +0100

    Started to add ImageViews and I have set the styles for the ImageViews
    Added all the ids/OnClickListener for the imageviews

--- app/src/main/java/com/jasonedwardes/websites/MainActivity.java
+++ app/src/main/java/com/jasonedwardes/websites/MainActivity.java
@@ -3,12 +3,69 @@ package com.jasonedwardes.websites;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.one);
+        btn1.setOnClickListener(this);
+        btn2 = findViewById(R.id.two);
+        btn2.setOnClickListener(this);
+        btn3 = findViewById(R.id.three);
+        btn3.setOnClickListener(this);
+        btn4 = findViewById(R.id.four);
+        btn4.setOnClickListener(this);
+        btn5 = findViewById(R.id.five);
+        btn5.setOnClickListener(this);
+        btn6 = findViewById(R.id.six);
+        btn6.setOnClickListener(this);
+        btn7 = findViewById(R.id.seven);
+        btn7.setOnClickListener(this);
+        btn8 = findViewById(R.id.eight);
+        btn8.setOnClickListener(this);
+        btn9 = findViewById(R.id.nine);
+        btn9.setOnClickListener(this);
+        btn10 = findViewById(R.id.ten);
+        btn10.setOnClickListener(this);
+        btn11 = findViewById(R.id.eleven);
+        btn11.setOnClickListener(this);
+        btn12 = findViewById(R.id.twelve);
+        btn12.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.one:
+                break;
+            case R.id.two:
+                break;
+            case R.id.three:
+                break;
+            case R.id.four:
+                break;
+            case R.id.five:
+                break;
+            case R.id.six:
+                break;
+            case R.id.seven:
+                break;
+            case R.id.eight:
+                break;
+            case R.id.nine:
+                break;
+            case R.id.ten:
+                break;
+            case R.id.eleven:
+                break;
+            case R.id.twelve:
+                break;
+        }
     }
 }
